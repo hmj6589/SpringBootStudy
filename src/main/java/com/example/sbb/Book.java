4ef8d7f1400233439caceb4fc982f767db1e0b3e
hmj6589 <hmj6589@gamil.com>
2024-07-23 01:08:33 +0900

    2장까지

---
+++ src/main/java/com/example/sbb/Book.java
@@ -0,0 +1,11 @@
+package com.example.sbb;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Book {
+    private String title;
+    private String author;
+}
---
+++ src/main/java/com/example/sbb/question/Question.java
@@ -0,0 +1,37 @@
+package com.example.sbb.question;
+
+import java.time.LocalDateTime;
+
+import com.example.sbb.answer.Answer;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.OneToMany;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+}
